22b165203b4a949247cf49e763c4bc09ddf0a9b8
BOU GHANNAM Nassim <nassimboughannam@gmail.com>
2018-01-03 13:56:43 +0200

    fix firebase dependency issue

--- src/app/authentication/service/Auth.service.ts
+++ src/app/authentication/service/Auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import * as firebase from 'firebase/app';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs/Observable';
+import {Observable} from "rxjs/Observable";
+import * as firebase from "firebase/app";
+import {AngularFireAuth} from "angularfire2/auth";
+
 
 @Injectable()
 export class AuthService {
